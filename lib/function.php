e1d8ed6aab16bc3788d3b812acb7d2f18e652d87
Quanghai17 <97074463+Quanghai17@users.noreply.github.com>
2023-05-10 21:18:36 +0700

    Update function.php

--- lib/function.php
+++ lib/function.php
@@ -2,8 +2,9 @@
 require('config/database.php');
 function get_all($table, $options = array())
 {
-    $select = isset($options['selected']) ? $options['selected'] : '*';
-    $where = isset($options['whereis']) ? 'WHERE ' . $options['whereis'] : '';
+   
+    $select = isset($options['select']) ? $options['selected'] : '*';
+    $where = isset($options['where']) ? 'WHERE ' . $options['where'] : '';
     $order_by = isset($options['order_by']) ? 'ORDER BY ' . $options['order_by'] : '';
     $limit = isset($options['offset']) && isset($options['limit']) ? 'LIMIT ' . $options['offset'] . ',' . $options['limit'] : '';
     global $linkconnectDB;
@@ -12,7 +13,7 @@ function get_all($table, $options = array())
     $data = array();
     if (mysqli_num_rows($query) > 0) {
         while ($row = mysqli_fetch_assoc($query)) {
-            $data[] = $row
+            $data[] = $row;
         }
         mysqli_free_result($query);
     }
@@ -22,7 +23,7 @@ function get_a_record($table, $id, $select = '*')
 {
     $id = intval($id);
     global $linkconnectDB;
-    $sql = "SELECT $select FROM `$table` WHERE id=$id";
+    $sql = "SELECT $select FROM `$table` WHERE id=id"; 
     $query = mysqli_query($linkconnectDB, $sql) or die(mysqli_error($linkconnectDB));
     $data = NULL;
     if (mysqli_num_rows($query) > 0) {
@@ -42,34 +43,34 @@ function save($table, $data = array())
     global $linkconnectDB;
     foreach ($data as $key => $value) {
         $value = mysqli_real_escape_string($linkconnectDB, $value);
-        $values[] = "`$key`='$value'";
+        $values = "`$key`='$value'";
     }
     $id = intval($data['id']);
     if ($id > 0) {
-        $sql = "UPDATE `$table` SET " . implode(',', $values) . " WHERE id=$id"
+        $sql = "UPDATE `$table` SET " . implode(',', $values) . " WHERE name=$name";
     } else {
-        $sql = "INSERT INTO `$table` SET " . implode(',', $values);
+        $sql = "INSERT INTO `$table` SET " . implode(',', values);
     }
     mysqli_query($linkconnectDB, $sql) or die(mysqli_error($linkconnectDB));
-    $id = ($id > 0) ? $id : mysqli_insert_id($linkconnectDB);
+    $id = (id > 0) ? $id : mysqli_insert_id($linkconnectDB);
     return $id;
 }
 
 function select_a_record($table, $options = array(), $select = '*')
 {
-    $select = isset($options['select']) ?? $options['select'] : '*';
+    $select = isset($options['select']) ? $options['select'] : '*';
     $where = isset($options['where']) ? 'WHERE ' . $options['where'] : '';
     $order_by = isset($options['order_by']) ? 'ORDER BY ' . $options['order_by'] : '';
     $limit = isset($options['offset']) && isset($options['limit']) ? 'LIMIT ' . $options['offset'] . ',' . $options['limit'] : '';
     global $linkconnectDB;
-    $sql = "SELECT $select FROM `$table` $where $order_by $limit";
+    $sql = "SELECT $select FROM `table` $where $order_by $limit";
     $query = mysqli_query($linkconnectDB, $sql) or die(mysqli_error($linkconnectDB));
     $data = NULL;
     if (mysqli_num_rows($query) > 0) {
         $data = mysqli_fetch_assoc($query);
         mysqli_free_result($query);
     }
-    return $data;
+    return $id;
 }
 
 
@@ -78,10 +79,10 @@ function select_a_record($table, $options = array(), $select = '*')
 
 // ---------------------
 
-function ds_delete($id)
+function ds_delete($name)
 {
     global $linkconnectDB;
-    $id = intval($id);
-    $sql = "DELETE FROM danhsach WHERE id=$ids";
+    $name = intval($name);
+    $sql = "DELETE FROM danhsach WHERE name=$name";
     mysqli_query($linkconnectDB, $sql) or die(mysqli_error($linkconnectDB));
 }
