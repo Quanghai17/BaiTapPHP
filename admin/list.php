5f8e706d3d3f702b4d406e1fed8e8d438027e1bf
anhtu1511 <tubg8787@gmail.com>
2023-05-10 21:58:27 +0700

    loi chinh ta

--- admin/list.php
+++ admin/list.php
@@ -13,7 +13,7 @@ $list_user = get_all('danhsach', $options);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Danh s?ch c?u h?i</title>
+    <title>Danh sách câu hỏi</title>
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="icon" href="../assets/img/Huy_Hiệu_Đoàn.png" type="image/gif" sizes="16x16">
@@ -21,9 +21,9 @@ $list_user = get_all('danhsach', $options);
 
 <body>
     <div class="container">
-        <h2>Danh sáh</h2>
+        <h2>Danh sách</h2>
         <br>
-        <a href="add.php" class="btn btn-primary">Thêm mớ </a>
+        <a href="add.php" class="btn btn-primary">Thêm mới</a>
         <br>
         <table class="table">
             <thead>
